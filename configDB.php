4491310fe60614cefeffe945a80c87d2839e62ac
Elina <112931233+EliMust@users.noreply.github.com>
2022-10-02 14:26:12 +0300

    Update configDB.php

--- configDB.php
+++ configDB.php
@@ -1,4 +1,4 @@
 <?php
 //подключение к БД
 $dsn = 'mysql:host=localhost; dbname=to-do';
-$pdo = new PDO($dsn, 'root', ''); //на удаленном хостинге провайдер выдает эти данные
+$pdo = new PDO($dsn, 'root', '');
